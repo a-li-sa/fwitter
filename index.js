23d085c886b1a381e943bdd79c4177086bd81c1b
Alisa Poon <alisa.poon97@gmail.com>
2020-09-30 21:04:00 -0700

    Created post and get routes for users

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/', routes);
 
 app.listen(3001, () => {
--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
+const connection = require('../../../config/connection.js');
 
 // api/users prepended to every Route
 router.route('/')
-  .get((_req, res) => {
-    res.send('Hello');
+  .get(async (_req, res) => {
+    const query = 'SELECT * FROM users;';
+    const [rows, fields] = await connection.query(query);
+    console.log('I AM ROWS', rows);
+    console.log('I AM FIELDS', fields);
+    res.json(rows);
+  })
+  .post(async (req, res) => {
+    const userInput = req.body;
+    const query = 'INSERT INTO users SET ?;';
+    // connection.query(query, userInput, (err) => {
+    //   if (err) throw err;
+    // });
+    // res.send(`added ${userInput.username} to database`);
+    const result = await connection.query(query, userInput);
+    res.json(result);
   });
 
 module.exports = router;
